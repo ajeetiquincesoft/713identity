c15b613a2f4661d2865d0d1df15f400b4a2470ec
ajayiquincesoft <ajay.iquincesoft@gmail.com>
2022-01-25 14:51:29 +0530

    treatment form

--- resources/views/admin/treatment/create.blade.php
+++ resources/views/admin/treatment/create.blade.php
@@ -116,6 +116,101 @@
                                     </div>
                                 </div>
                             </div>
+							
+							<!-- treatment option form start--->
+							
+							<div class="row">
+								<div class="col-lg-9"></div>
+								<div class="col-lg-3">
+									<div class="addtreamentoption btn-dark">Add Treatment Option</div>
+								</div>
+							</div>
+							
+							<div class="treatmentoption">
+								<div class="row">
+								 <div class="card-header">
+									<div class="row align-items-center">
+										<div class="col-12">
+											<h3 class="mb-0">Treatment Option</h3>
+										</div>
+									</div>
+								  </div>
+								</div>
+								<div class="row">
+									<div class="col-lg-12">
+										<div class="form-group">
+											<label class="form-control-label">Title</label>
+											<input type="text" name="treamenttitle" class="form-control" value="{{ old('treamenttitle') }}">
+											@error('treamenttitle')<div class="text-danger">{{ $message }}*</div>@enderror
+										</div>
+									</div>
+									</div>
+									<div class="row">
+									<div class="col-lg-12">
+										<div class="form-group">
+											<label class="form-control-label">Image</label>
+											<input type="file" name="timage">
+											@error('timage')<div class="text-danger">{{ $message }}*</div>@enderror
+										</div>
+									</div>
+								</div>
+								<div class="row">
+								 <div class="card-header">
+									<div class="row align-items-center">
+										<div class="col-12">
+											<h3 class="mb-0">Package</h3>
+										</div>
+									</div>
+								  </div>
+								</div>
+								<div class="row">
+									<div class="col-lg-12">
+										<div class="form-group">
+											<label class="form-control-label">Title</label>
+											<input type="text" name="packagename" class="form-control" value="{{ old('packagename') }}">
+											@error('packagename')<div class="text-danger">{{ $message }}*</div>@enderror
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-lg-12">
+										<div class="form-group">
+											<label class="form-control-label">Package Type</label>
+											 <select name="packagetype" class="form-control">
+													<option value="">Select Package Type</option>
+													<option value="front">Front</option>
+													<option value="back">Back</option>
+											 </select>
+
+                                        @error('status')<div class="text-danger">{{ $message }}*</div>@enderror
+										</div>
+									</div>
+								</div>
+								<div class="row">
+									<div class="col-lg-4">
+										<div class="form-group">
+											<label class="form-control-label">Sub Package</label>
+											 <input type="text" name="subpackage" class="form-control" value="{{ old('subpackage') }}">
+										</div>
+									</div>
+									<div class="col-lg-3">
+										<div class="form-group">
+											<label class="form-control-label">Price</label>
+											 <input type="text" name="price" class="form-control" value="{{ old('price') }}">
+										</div>
+									</div>
+									<div class="col-lg-3">
+										<div class="form-group">
+											<label class="form-control-label">Unit<small>(hint: max-13 and min-10)</small></label>
+											 <input type="text" name="subpackage" class="form-control" value="{{ old('subpackage') }}">
+										</div>
+									</div>
+									<div class="col-lg-2">
+										<a href="javascript:void(0);" class="add_button" title="Add field"><img src="add-icon.png"/></a>
+									</div>
+								</div>
+							</div>
+							<!--------end of treatmentoption ------->
                             <div class="row">
                                 <div class="col-lg-4">
                                     <button type="submit" class="btn btn-dark">Submit</button>
@@ -130,5 +225,13 @@
         </div>
     </div>
 </div>
-
 @endsection
+@section('scripts')
+<script>
+jQuery(document).ready(function(){
+  jQuery(".addtreamentoption").click(function(){
+    alert("The paragraph was clicked.");
+  });
+});
+</script>
+@stop
