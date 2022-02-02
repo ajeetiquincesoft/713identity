b7c7ef5e8531270f3f9697d47ff17d2a7305b115
ajeetiquincesoft <ajeet.iquincesoft@gmail.com>
2022-02-02 17:05:33 +0530

    testing

--- app/Http/Controllers/Admin/UnavailabilityController.php
+++ app/Http/Controllers/Admin/UnavailabilityController.php
@@ -89,7 +89,7 @@ class UnavailabilityController extends Controller
         $time_slotes = array('08:00', '08:30', '09:00', '09:30', '10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00', '20:30');
 
         $data = Unavailability::find($id);
-        return view('admin/unavailability/edit', compact('data','time_slotes'));
+        return view('admin/unavailability/edit', compact('data','time_slotes')); 
     }
 
     /**
